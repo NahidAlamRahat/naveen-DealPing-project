7c03472de57c22ed2ad9e869b9a5838550ab6846
Md. Sayeed Hassan Maheen <sayeedhassan1124@gmail.com>
2025-03-19 15:19:15 +0600

    19.03.25

---
+++ lib/main_app_entry.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      AppSize.size =
+          MediaQueryData.fromView(WidgetsBinding.instance.window).size;
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppStrings.appName,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.green500),
+          useMaterial3: true,
+          fontFamily: AppStrings.fontFamilyName,
+        ),
+        initialRoute: RouteManager.initial,
+        getPages: RouteManager.getPages(),
+      );
+    });
+  }
+}
