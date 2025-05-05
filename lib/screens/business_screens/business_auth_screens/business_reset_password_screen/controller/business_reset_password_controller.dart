bb68296cd35e0c6381e3da932f80ce17cef8834f
Md. Sayeed Hassan Maheen <sayeedhassan1124@gmail.com>
2025-05-05 21:52:44 +0600

    Business Forgot password, Forgot password verify, Reset password API implemented

--- lib/screens/business_screens/business_auth_screens/business_reset_password_screen/controller/business_reset_password_controller.dart
+++ lib/screens/business_screens/business_auth_screens/business_reset_password_screen/controller/business_reset_password_controller.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../../routes/app_routes.dart';
+import '../../../../../services/repository/auth_repository/auth_repository.dart';
+import '../../../../../widgets/app_snack_bar/app_snack_bar.dart';
 
 class BusinessResetPasswordController extends GetxController {
   final newPasswordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
+
+  late String token;
+
+  @override
+  void onInit() {
+    super.onInit();
+    if (Get.arguments != null && Get.arguments is Map<String, dynamic>) {
+      token = Get.arguments['token'] ?? '';
+    } else {
+      token = '';
+    }
+  }
 
   @override
   void onClose() {
@@ -14,9 +28,33 @@ class BusinessResetPasswordController extends GetxController {
     super.onClose();
   }
 
-  void reset() {
-    // Implement your OTP logic here
-    // If OTP is successfully received, navigate to the new password screen
-    Get.offAllNamed(AppRoutes.businessSignInScreen);
+  void reset() async {
+    if (newPasswordController.text.isEmpty ||
+        confirmPasswordController.text.isEmpty) {
+      AppSnackBar.error("Please fill in all required fields.");
+      return;
+    }
+
+    if (newPasswordController.text != confirmPasswordController.text) {
+      AppSnackBar.error("Passwords do not match.");
+      return;
+    }
+
+    try {
+      bool isSuccess = await AuthRepository().resetPassword(
+        newPassword: newPasswordController.text,
+        confirmPassword: confirmPasswordController.text,
+        token: token,
+      );
+
+      if (isSuccess) {
+        AppSnackBar.success("Password reset successfully.");
+        Get.offAllNamed(AppRoutes.businessSignInScreen);
+      } else {
+        AppSnackBar.error("Failed to reset password. Please try again.");
+      }
+    } catch (e) {
+      AppSnackBar.error("An error occurred. Please try again.");
+    }
   }
 }
